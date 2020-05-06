e90012bec8ace14bbca63b6e827380ebc2321dfb
siva <siva3060@gmail.com>
2020-05-06 11:41:20 +0530

    Repo Clean up

--- src/App.js
+++ src/App.js
@@ -53,7 +53,7 @@ class App extends Component {
 		const data = new FormData();
 		data.append('upload_file',this.state.selectedFile[0]);
 		//use the axios API to send the file 
-		axios.post("https://15.188.74.126/upload",data).then(res => {
+		axios.post("https://15.188.74.126/Upload",data).then(res => {
 			//console.log("File has been uploaded sucessfully")
 			console.log(res.statusTest);
 			alert("File has been uploaded sucessfully");
